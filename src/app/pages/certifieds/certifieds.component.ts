86ad2a773a431ef5c59fa7ca6bccac4c432bd3fb
Alexandre-Gaspar <alexandretiao16@gmail.com>
2025-09-26 14:48:11 +0100

    ✨ Feat: add the second state certificates page

--- src/app/pages/certifieds/certifieds.component.ts
+++ src/app/pages/certifieds/certifieds.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+import { CertifiedItemComponent } from "../../_components/certified-item/certified-item.component";
 
 @Component({
   selector: 'app-certifieds',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, CertifiedItemComponent],
   templateUrl: './certifieds.component.html',
   styleUrl: './certifieds.component.css'
 })
