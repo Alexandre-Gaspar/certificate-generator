f15b2e2673c4287e34ae6de580a3ef1a08ef5746
Alexandre-Gaspar <alexandretiao16@gmail.com>
2025-09-26 13:40:22 +0100

    ✨ Feat: add certified-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { CertifiedItemComponent } from './_components/certified-item/certified-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
