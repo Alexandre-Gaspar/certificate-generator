5f922dcd4272748734d0990476b6a1d33580c972
Alexandre-Gaspar <alexandretiao16@gmail.com>
2025-09-26 14:40:15 +0100

    ✨ Feat: add the first state certifieds list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
 import { CertifiedItemComponent } from './_components/certified-item/certified-item.component';
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertifiedsComponent } from "./pages/certifieds/certifieds.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent, BaseUiComponent, CertifiedsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/certifieds/certifieds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+
+@Component({
+  selector: 'app-certifieds',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './certifieds.component.html',
+  styleUrl: './certifieds.component.css'
+})
+export class CertifiedsComponent {
+
+}
