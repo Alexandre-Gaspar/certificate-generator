4b15e65ce82e05bdf28dd4a660b44904a850e364
Alexandre Gaspar <alexandretiao16@gmail.com>
2025-10-05 13:55:02 +0100

    ✨feat(certificate-form): add validation in thje fields

--- src/app/pages/certificate-form/certificate-form.component.ts
+++ src/app/pages/certificate-form/certificate-form.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificate-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificate-form.component.html',
   styleUrl: './certificate-form.component.css'
 })
 export class CertificateFormComponent {
+  name: string = '';
+  activity: string = '';
+  activities: string[] = [];
 
+  validateField(field: NgModel) {
+    return field.invalid && field.touched;
+  }
 }
